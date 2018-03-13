946cc56d05d207b3643a7729a2bc3915de422e92
Andreas Alme <andreas.alme.eriksson@gmail.com>
2018-03-13 20:36:27 +0100

    add windows support

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # compile and run
-javac -cp .:lib/java-json.jar Main.java
+
+PATHSEP=":"
+if [[ $OS == "Windows_NT" ]]
+then
+    PATHSEP=";"
+fi
+
+javac -cp .${PATHSEP}lib/java-json.jar Main.java
 if [ $# -eq 1 ];
 then
-    java -cp .:lib/java-json.jar Main $1
+    java -cp .${PATHSEP}lib/java-json.jar Main $1
 elif [ $# == 0 ];
 then
-    java -cp .:lib/java-json.jar Main
+    java -cp .${PATHSEP}lib/java-json.jar Main
 fi
